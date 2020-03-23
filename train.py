ef5e34bc456c437fe6cd35cb0694e2663604a4ee
Pavel Nesterov <pavel.a.nesterov@gmail.com>
2020-03-23 06:03:06 +0100

    Add validation loop to each epoch epock log diplay

--- train.py
+++ train.py
@@ -11,11 +11,26 @@ def get_input_args():
     parser.add_argument('--gpu', type = bool, default = False)
     
     in_args = parser.parse_args()
+    #print(type(in_args.gpu))
     print("Argument 1:", in_args.data_dir)
     print("Argument 2:", in_args.arch)
     print("Argument 3:", in_args.learning_rate)
     return in_args
 
-print("Train aruments successfully imported")
+def get_hardcoded_input_args():
+    data_dir = 'flower_data/' 
+    save_dir = 'checkpoints/'
+    arch = 'vgg13'
+    learning_rate = 0.01
+    hidden_units = 512
+    epochs = 30
+    gpu = False
+    in_args = [data_dir, save_dir, arch, learning_rate, hidden_units, epochs, gpu]
+    #print(in_args)
+    return in_args
+
+
+#print("Train aruments successfully imported")
 
 #get_input_args()
+#get_hardcoded_input_args()
